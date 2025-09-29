eb019dafe41bb232fb7c189b77ab688067143703
bmorenobrayanne <bmoreno53@uan.edu.co>
2025-09-28 21:02:46 -0500

    Aplicacion MVVM

--- app/src/main/java/com/example/aplicacioncamara/view/MainActivity.kt
+++ app/src/main/java/com/example/aplicacioncamara/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.aplicacioncamara
+package com.example.aplicacioncamara.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.aplicacioncamara.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
